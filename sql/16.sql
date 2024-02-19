10e000dc41d0008160926352b11c5318e99f1baa
Manan Mendiratta <123344066+mmendiratta27@users.noreply.github.com>
2024-02-19 15:14:11 -0800

    Fixed Typo in 16.sql

--- sql/16.sql
+++ sql/16.sql
@@ -1,5 +1,6 @@
 /*
  * Use a JOIN to order the films by most profitable,
- * where the profit is the total amount that customer have payer for the film.
+ * where the profit is the total amount that customers have paid for the film.
  * Use tables payment, rental, inventory, and film. 
  */
+
